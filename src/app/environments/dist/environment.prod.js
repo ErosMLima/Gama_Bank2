5a31b19f899db97cf3c7d8501ad16432b5d14769
Eros M.Lima <60955355+ErosMLima@users.noreply.github.com>
2021-04-03 17:17:02 -0300

    Fake Bank src folder part III (#009)
    Fake Bank src folder part III (#009)

---
+++ src/app/environments/dist/environment.prod.js
@@ -0,0 +1,7 @@
+"use strict";
+exports.__esModule = true;
+exports.environment = void 0;
+exports.environment = {
+    production: true,
+    API_URL: 'https://accenture-java-desafio.herokuapp.com'
+};
