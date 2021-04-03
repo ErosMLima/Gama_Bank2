3e214dfbc3cb9f4432108d48a1d47c3eaa995130
Eros M.Lima <60955355+ErosMLima@users.noreply.github.com>
2021-04-03 17:29:35 -0300

    Delete 2
    Delete 2

--- src/app/environments/dist/environment.prod.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.environment = void 0;
-exports.environment = {
-    production: true,
-    API_URL: 'https://accenture-java-desafio.herokuapp.com'
-};
--- src/app/environments/environment.prod.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: true,
-  API_URL: 'https://accenture-java-desafio.herokuapp.com',
-};
